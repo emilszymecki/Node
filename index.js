10eb13d88e58ff631a9163b2825c62fd9fab5d31
Emil <emilszymecki@gmail.com>
2017-07-21 16:35:58 +0200

    test

---
+++ index.js
@@ -0,0 +1,33 @@
+var BlogController = function() {
+  var index = function(posts) {
+    return console.log(posts);
+  };
+
+  var show = function(post) {
+    return Views.show(post);
+  };
+
+  var create = function(attrs) {
+    return Db.create(attrs);
+  };
+
+  var update = function(post, attrs) {
+    return Db.update(post, attrs);
+  };
+
+  var destroy = function(post) {
+    return Db.destroy(post);
+  };
+
+  return {
+    index: index,
+    show: show,
+    create: create,
+    update: update,
+    destroy: destroy,
+  };
+};
+
+
+var z = BlogController()
+z.index("dupa")
